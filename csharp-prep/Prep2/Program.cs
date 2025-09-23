a0911921614deca967e966b43ded2f82b0b029b5
SROGLE <spencerghost09@gmail.com>
2025-09-23 13:14:45 -0600

    prep 1 and 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is you grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percent <= 100 && percent >= 0)
+        {
+            Console.WriteLine($"Your grade is {letter}.");
+        }
+        else
+        {
+            Console.WriteLine("You did not enter a valid grade percentage.");
+        }
     }
 }
